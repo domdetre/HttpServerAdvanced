361d8d1146d38bddd4a25de407f13a3351741c95
Detre Dominus <dominusdetre@gmail.com>
2019-04-28 01:44:07 +0100

    Adding DELETE as allowed method to cors

--- HttpResponse.cpp
+++ HttpResponse.cpp
@@ -30,7 +30,7 @@ String HttpResponse::toString()
     "Content-Type: " + contentType + "\r\n" +
     "Access-Control-Allow-Headers: *\r\n" +
     "Access-Control-Allow-Origin: *\r\n" +
-    "Access-Control-Allow-Methods: GET, POST, PUT\r\n" + 
+    "Access-Control-Allow-Methods: GET, POST, PUT, DELETE\r\n" + 
     "Access-Control-Expose-Headers: HSA-Version\r\n" +
     "HSA-Version: " + String(HTTP_SERVER_ADVANCED_VERSION) + "\r\n" +
     "\r\n" + data + "\r\n";
